38e48e345eb77b4ccdc604ad908396bba60cf14e
Phil Peterson <phpeterson@usfca.edu>
2020-09-27 17:54:35 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # autograder
-Clone and build repos from Github Classroom. Run test cases vs. expected output
+1. Clone and build repos from Github Classroom. 
+2. Run test cases vs. expected output
+3. Generate a score based on a rubric for each test case
+
+## Requirements
+1. Requires python3
+1. For JSON, remember that there's no comma on the last object. If you include a 
+comma after the last object, you'll get a syntax error parsing the JSON file.
+
+## Usage for students
+You will need a file called config.json which contains the Github Classroom organization
+name and the project name for each project you work on. Example
+<pre><code>
+pi@raspberrypi:~/phpeterson-usf/autograder $ cat config.json
+{
+    "org": "cs-315-03-20f",
+    "project": "project02"
+}
+</code></pre>
+<pre><code>
+$ git clone https://github.com/phpeterson-usf/autograder.git
+$ cd autograder
+$ python3 autograder.py ~/project02
+/home/pi/project02 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 100
+</code></pre>
+
+## Usage for instructors
+1. Test cases for each project are expressed in JSON, as you can see in the `tests/` directory
+1. Test case inputs are a list of strings for each command-line flag and value, since
+that's how python subprocess takes arguments. Maybe there's a better way
+    <pre><code>pi@raspberrypi:~/phpeterson-usf/autograder/tests $ cat project02.json
+    {
+        "tests": [
+            {
+                "name": "01",
+                "input": ["-e", "1 + 1"],
+                "expected": "2",
+                "rubric": 5
+            },
+            {
+                "name": "02",
+                "input": ["-e", "10", "-b", "16"],
+                "expected": "0x0000000A",
+                "rubric": 5
+            }
+        ]
+    }</code></pre>
+1. You can make up a JSON file with your list of students, and autograder will loop
+over the list, cloning, building, and testing each one. Each student object contains:
+    1. A property called "github" for the student's Github ID
+    2. A property called "login" for mapping the student's Github ID to a Canvas ID, 
+    as used at usfca.edu where I teach
+        <pre><code>pi@raspberrypi:~/phpeterson-usf/autograder $ cat students.json
+        {
+            "students": [
+                {
+                    "github": "phpeterson-usf",
+                    "login": "test"
+                },
+                {
+                    "github": "gdbenson",
+                    "login": "test"
+                }
+            ]
+        }</code></pre>
---
+++ autograder.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import subprocess
+import sys
+
+
+def json_load(path):
+    try:
+        with open(path) as json_file:
+            return json.load(json_file)
+    except FileNotFoundError:
+        print('FileNotFound: ' + path)
+        return {}
+
+
+def tests_load(cfg):
+    tests_fname = cfg['project'] + '.json'
+    tests_path = os.path.join('tests', tests_fname)
+    return json_load(tests_path)
+
+
+def cmd_exec(args):
+    return subprocess.run(args, capture_output = True, timeout = 5)
+
+
+def cmd_exec_rc(args):
+    proc = cmd_exec(args)
+    return proc.returncode
+
+
+def cmd_exec_stdout(args):
+    proc = cmd_exec(args)
+    return proc.stdout.decode('utf-8').rstrip('\n')
+
+
+def repo_path(cfg, student):
+    student_dir = cfg['project'] + '-' + student['github']
+    return os.path.join('github.com', cfg['org'], student_dir)
+
+
+def repo_clone(repo, cfg, student):
+    if os.path.isdir(repo):
+        return 0;
+    remote = 'https://github.com/' + cfg['org'] + '/' + cfg['project'] + '-' + student['github']
+    return cmd_exec_rc(['git', 'clone', remote, repo])
+
+
+def repo_build(repo):
+    return cmd_exec_rc(['make', '-C', repo])
+
+
+def print_green(s):
+    print('\033[92m' + s + ' \033[0m', end = '')
+
+
+def print_red(s):
+    print('\033[91m' + s + ' \033[0m', end = '')
+
+
+def repo_test(repo, cfg, tests):
+    score = 0;
+    executable = repo + '/' + cfg['project']
+    
+    for test in tests['tests']:
+        args = [i for i in test['input']]
+        args.insert(0, executable)
+        if cmd_exec_stdout(args).lower() == test['expected'].lower():
+            print_green(test['name'])
+            score += test['rubric']
+        else:
+            print_red(test['name'])
+    return score
+
+
+def repo_build_test(repo, cfg, tests):
+    if repo_build(repo) != 0:
+        print('build failed')
+        return False, 0
+    else:
+        return True, repo_test(repo, cfg, tests)
+
+
+def main(argv):
+    cfg = json_load('config.json')
+    students = json_load('students.json')
+    tests = tests_load(cfg)
+    if tests == {} or cfg == {}:
+        return -1
+
+    # One local repo: build and test without cloning
+    if len(argv) == 2:
+        repo = argv[1]
+        if not os.path.isdir(repo):
+            print(repo + ' is not a directory')
+            return -1
+        print(repo + ' ', end = '')
+        ok, score = repo_build_test(repo, cfg, tests)
+        if ok == True:
+            print(score)
+        return 0
+
+    # Calc column width for justified printing
+    longest_github = 0;
+    for student in students['students']:
+        l = len(student['github'])
+        if l > longest_github:
+            longest_github = l
+    longest_github += 1
+
+    for student in students['students']:
+
+        # Justify columns by width of github ID
+        print(student['github'], end = '')
+        for i in range (longest_github - len(student['github'])):
+            print(' ', end = '')
+
+        # Clone
+        repo = repo_path(cfg, student)
+        if repo_clone(repo, cfg, student) != 0:
+            print('clone failed')
+            continue
+
+        # Build and test
+        ok, score = repo_build_test(repo, cfg, tests)
+        if ok == False:
+            continue
+        print(score)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
