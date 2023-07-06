e483690f6357e8838e1e9e989ae547b0bc5f6bc4
colmpat <cplang@dons.usfca.edu>
2023-07-06 11:03:52 -0700

    switch github action code to grab artifact json

--- actions/github.py
+++ actions/github.py
@@ -40,82 +40,73 @@ class Github(Server):
                 self.github_cfg.host_name, self.org, self.project, student)
         return url
 
-
-    def make_log_url(self, student, run_id):
-        url = self.make_action_runs_url(student)
-        url += f'/{run_id}/logs'
+    def make_repo_artifacts_url(self, student):
+        url = 'https://{}/repos/{}/{}-{}/actions/artifacts'.format(
+                self.github_cfg.host_name, self.org, self.project, student)
         return url
 
 
-    def get_first_action_run(self, student):
-        # Get all of the workflow runs
-        url = self.make_action_runs_url(student)
-        runs = self.get_url(url, self.github_headers())
+    def get_first_artifact_for_repo(self, student):
+        # Get all artifacts for the workflow run
+        url = self.make_repo_artifacts_url(student)
+        artifacts = self.get_url(url, self.github_headers())
 
-        run = {}
+        artifact = {}
         try:
-            # The first run in the workflow_runs list is the most recent
-            run = runs['workflow_runs'][0]
+            # The first artifact in the artifacts list is the most recent
+            artifact = artifacts['artifacts'][0]
         except Exception as e:
-            warn('Accessing first workflow run: ' + str(e))
-        return run
+            warn('Accessing first artifact: ' + str(e))
+        return artifact
 
-
-    def get_action_run_summary_url(self, student, run):
+    def get_action_run_summary_url(self, student, artifact):
         run_url = self.make_action_runs_url(student)
-        jobs_url = run_url + f'/{run["id"]}/jobs'
+        run_id = artifact['workflow_run']['id']
+        jobs_url = run_url + f'/{run_id}/jobs'
         jobs = self.get_url(jobs_url, self.github_headers())
         if jobs:
             job_id = jobs['jobs'][0]['id']
             return 'https://github.com/{}/{}-{}/actions/runs/{}#summary-{}'.format(
-                self.org, self.project, student, run['id'], job_id)
+                self.org, self.project, student, run_id, job_id)
         else:
-            w = f'No jobs found for {student} run_id: {run["id"]}'
+            w = f'No jobs found for {student} run_id: {run_id}'
             warn(w)
             return w
 
 
-    def get_action_run_score(self, student, run):
-        score = None
-        # Download the logs for the most recent run
-        url = self.make_log_url(student, run['id'])
-        log_zip = self.get_url(url, self.github_headers())
+    def get_artifact_results(self, artifact):
+        results = None
+        # Download the artifact
+        url = artifact['archive_download_url']
+        artifact_zip = self.get_url(url, self.github_headers())
 
         # The logs for the workflow run are in zip format
         try:
-            with ZipFile(BytesIO(log_zip)) as log_file:
-                log_text = log_file.read('1_Autograder Results.txt').decode('utf-8')
-            for line in log_text.split('\n'):
-                # Split off the timestamp part of the line
-                line_parts = line.split(' ', 1)
-                if len(line_parts) > 1:
-                    second_part = line_parts[1]
-                    score_parts = second_part.split('=', 1)
-                    # Find the line which specifies the score
-                    if score_parts[0] == 'FINAL_SCORE':
-                        # Split off the earned points from the possible points
-                        score = score_parts[1].split('/')[0]
+            with ZipFile(BytesIO(artifact_zip)) as log_file:
+                results = log_file.read('grade-results.json')
+                results = json.loads(results.decode('utf-8'))
+                results['grade'] = float(results['grade'])
         except Exception as e:
-            log_text = 'Analyzing action log: ' + str(e)
+            log_text = 'Analyzing artifact: ' + str(e)
             warn(log_text)
 
-        if score == None:
-            warn('No score found in action log')
-        return score
-
+        if results == None:
+            warn('No results found in artifact')
+        return results
 
     def get_action_results(self, student):
         repo_result = init_repo_result(student)
     
-        # Ask github.com for the first action run
-        run = self.get_first_action_run(student)
-        if run:
-            # Ask github.com for the log for that workflow run
-            repo_result['score'] = self.get_action_run_score(student, run)
+        # Ask github.com for the first artifact run
+        artifact = self.get_first_artifact_for_repo(student)
+        if artifact:
+            # Download the artifact and extract the grade
+            results = self.get_artifact_results(artifact)
+            repo_result['score'] = results['grade']
             # Calculate the web browser (not "api) URL for the first job ID in the action run
-            repo_result['comment'] = self.get_action_run_summary_url(student, run)
+            repo_result['comment'] = self.get_action_run_summary_url(student, artifact)
         else:
-            log_text = 'No workflow runs found'
+            log_text = 'No artifacts found'
             warn(log_text)
 
         # Notice no test case results are generated here. The idea is that
